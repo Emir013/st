8c9ac3665352ff0da7917fa51eb5145c1ca84bb5
Emir013 <emir.mulahalilovic@live.com>
2020-10-13 17:31:22 +0200

    config

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "DejaVu Sans  Mono:pixelsize=14:antialias=true:autohint=true";
+static char *font = "DejaVuSansMono Nerd Font:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
